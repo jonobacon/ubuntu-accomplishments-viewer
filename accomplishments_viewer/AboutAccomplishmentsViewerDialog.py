d4fb4939f33d32b76d7ff5b9453029b8d4514b93
Jono Bacon <jono@ubuntu.com>
2012-05-17 16:51:23 -0700

    * Fixed bug that was breaking the about box in glade.  * Added accomplishment authors to credits box.

--- accomplishments_viewer/AboutAccomplishmentsViewerDialog.py
+++ accomplishments_viewer/AboutAccomplishmentsViewerDialog.py
@@ -10,6 +10,8 @@ locale.bindtextdomain('accomplishments-viewer', locale_dir)
 gettext.bindtextdomain('accomplishments-viewer',locale_dir)
 gettext.textdomain('accomplishments-viewer')
 
+from accomplishments.daemon import dbusapi
+
 import logging
 logger = logging.getLogger('accomplishments_viewer')
 
@@ -25,3 +27,16 @@ class AboutAccomplishmentsViewerDialog(AboutDialog):
 
         # Code for other initialization actions should be added here.
 
+        self.libaccom = dbusapi.Accomplishments()
+
+        # add app authors
+        authors = [ "Jono Bacon <jono@ubuntu.com>", "Stuart Langridge <sil@kryogenix.org>"]
+        
+        authors.append(" ")
+        authors.append(_("Accomplishment Authors:"))
+        authors.append(" ")
+
+        for a in self.libaccom.get_collection_authors("ubuntu-community"):
+            authors.append(a)
+            
+        self.set_authors(authors)
