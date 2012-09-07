c81ac2f73ec659e2044a12e2d9597ff119c01787
Jono Bacon <jono@ubuntu.com>
2012-09-07 11:19:02 -0700

    * Quickly upgrade.

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 ### BEGIN LICENSE
-# This file is in the public domain
+# Copyright (C) 2012 Jono Bacon <jono@ubuntu.com>
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License version 3, as published 
+# by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranties of 
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+# PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along 
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
 ### END LICENSE
 
 ###################### DO NOT TOUCH THIS (HEAD TO THE SECOND PART) ######################
@@ -16,12 +27,13 @@ except ImportError:
     sys.exit(1)
 assert DistUtilsExtra.auto.__version__ >= '2.18', 'needs DistUtilsExtra.auto >= 2.18'
 
-def update_config(values = {}):
+def update_config(libdir, values = {}):
 
+    filename = os.path.join(libdir, 'accomplishments_viewer_lib/accomplishments_viewerconfig.py')
     oldvalues = {}
     try:
-        fin = file('accomplishments_viewer_lib/accomplishments_viewerconfig.py', 'r')
-        fout = file(fin.name + '.new', 'w')
+        fin = file(filename, 'r')
+        fout = file(filename + '.new', 'w')
 
         for line in fin:
             fields = line.split(' = ') # Separate variable from value
@@ -35,39 +47,87 @@ def update_config(values = {}):
         fin.close()
         os.rename(fout.name, fin.name)
     except (OSError, IOError), e:
-        print ("ERROR: Can't find accomplishments_viewer_lib/accomplishments_viewerconfig.py")
+        print ("ERROR: Can't find %s" % filename)
         sys.exit(1)
     return oldvalues
 
 
-def update_desktop_file(datadir):
+def move_desktop_file(root, target_data, prefix):
+    # The desktop file is rightly installed into install_data.  But it should
+    # always really be installed into prefix, because while we can install
+    # normal data files anywhere we want, the desktop file needs to exist in
+    # the main system to be found.  Only actually useful for /opt installs.
+
+    old_desktop_path = os.path.normpath(root + target_data +
+                                        '/share/applications')
+    old_desktop_file = old_desktop_path + '/accomplishments-viewer.desktop'
+    desktop_path = os.path.normpath(root + prefix + '/share/applications')
+    desktop_file = desktop_path + '/accomplishments-viewer.desktop'
+
+    if not os.path.exists(old_desktop_file):
+        print ("ERROR: Can't find", old_desktop_file)
+        sys.exit(1)
+    elif target_data != prefix + '/':
+        # This is an /opt install, so rename desktop file to use extras-
+        desktop_file = desktop_path + '/extras-accomplishments-viewer.desktop'
+        try:
+            os.makedirs(desktop_path)
+            os.rename(old_desktop_file, desktop_file)
+            os.rmdir(old_desktop_path)
+        except OSError as e:
+            print ("ERROR: Can't rename", old_desktop_file, ":", e)
+            sys.exit(1)
+
+    return desktop_file
+
+def update_desktop_file(filename, target_pkgdata, target_scripts):
 
     try:
-        fin = file('accomplishments-viewer.desktop.in', 'r')
-        fout = file(fin.name + '.new', 'w')
+        fin = file(filename, 'r')
+        fout = file(filename + '.new', 'w')
 
-        for line in fin:            
+        for line in fin:
             if 'Icon=' in line:
-                line = "Icon=%s\n" % (datadir + 'media/accomplishments-viewer.svg')
+                line = "Icon=%s\n" % (target_pkgdata + 'media/accomplishments-viewer.svg')
+            elif 'Exec=' in line:
+                cmd = line.split("=")[1].split(None, 1)
+                line = "Exec=%s" % (target_scripts + 'accomplishments-viewer')
+                if len(cmd) > 1:
+                    line += " %s" % cmd[1].strip()  # Add script arguments back
+                line += "\n"
             fout.write(line)
         fout.flush()
         fout.close()
         fin.close()
         os.rename(fout.name, fin.name)
     except (OSError, IOError), e:
-        print ("ERROR: Can't find accomplishments-viewer.desktop.in")
+        print ("ERROR: Can't find %s" % filename)
         sys.exit(1)
+
+def compile_schemas(root, target_data):
+    if target_data == '/usr/':
+        return  # /usr paths don't need this, they will be handled by dpkg
+    schemadir = os.path.normpath(root + target_data + 'share/glib-2.0/schemas')
+    if (os.path.isdir(schemadir) and
+            os.path.isfile('/usr/bin/glib-compile-schemas')):
+        os.system('/usr/bin/glib-compile-schemas "%s"' % schemadir)
 
 
 class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
     def run(self):
-        values = {'__accomplishments_viewer_data_directory__': "'%s'" % (self.prefix + '/share/accomplishments-viewer/'),
-                  '__version__': "'%s'" % self.distribution.get_version()}
-        previous_values = update_config(values)
-        update_desktop_file(self.prefix + '/share/accomplishments-viewer/')
         DistUtilsExtra.auto.install_auto.run(self)
-        update_config(previous_values)
 
+        target_data = '/' + os.path.relpath(self.install_data, self.root) + '/'
+        target_pkgdata = target_data + 'share/accomplishments-viewer/'
+        target_scripts = '/' + os.path.relpath(self.install_scripts, self.root) + '/'
+
+        values = {'__accomplishments_viewer_data_directory__': "'%s'" % (target_pkgdata),
+                  '__version__': "'%s'" % self.distribution.get_version()}
+        update_config(self.install_lib, values)
+
+        desktop_file = move_desktop_file(self.root, target_data, self.prefix)
+        update_desktop_file(desktop_file, target_pkgdata, target_scripts)
+        compile_schemas(self.root, target_data)
 
         
 ##################################################################################
