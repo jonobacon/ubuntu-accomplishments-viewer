c5591714afc313c969cf162cd6627e8a675ed935
Matthew Fischer <matthew.fischer@canonical.com>
2012-10-20 21:17:20 -0600

    adding a manpage

--- setup.py
+++ setup.py
@@ -136,7 +136,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='accomplishments-viewer',
-    version='0.1',
+    version='0.3',
     license='GPL-3',
     author='Jono Bacon',
     author_email='jono@ubuntu.com',
